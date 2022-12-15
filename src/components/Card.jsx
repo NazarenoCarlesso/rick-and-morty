12e32e994a1e30228ea86a90a8cc4fc911986d52
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2022-12-15 15:14:32 -0300

    fix and styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,17 @@
+import styles from './Card.module.css';
+
 export default function Card(props) {
    return (
-      <div>
-         <button onClick={props.onClose}>X</button>
-         <h2>{props.name}</h2>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
+      <div className={styles.divCard}>
+         <button className={styles.buttonClose} onClick={props.onClose}>X</button>
+         <div className={styles.divName}>
+            <h2 className={styles.h2Name}>{props.name}</h2>
+         </div>
          <img  src={props.image} alt="" />
+         <div className={styles.divProps}>
+            <h2 className={styles.h2Species}>{props.species}</h2>
+            <h2 className={styles.h2Gender}>{props.gender}</h2>
+         </div>
       </div>
    );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,12 @@
 import Card from './Card';
+import styles from './Cards.module.css';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div>
+   return <div className={styles.div}>
       {characters.map(character => {
-         const { name, species, genre, image } = character // falta onClose
-         return (<Card name={name} species={species} genre={genre} image={image} onClose={() => window.alert('Emulamos que se cierra la card')}></Card>)
+         const { name, species, gender, image } = character // falta onClose
+         return (<Card name={name} species={species} gender={gender} image={image} onClose={() => window.alert('Emulamos que se cierra la card')}></Card>)
       })}
    </div>;
 }
